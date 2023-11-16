5eccabb81346b90e1686874158a89b411bd21e22
Max Neunhoeffer <max@arangodb.com>
2023-11-16 15:38:35 +0100

    Fixes.

--- src/stringbench.cpp
+++ src/stringbench.cpp
@@ -14,29 +14,6 @@ void test(void* arena, char* start, char* size, char* target);
 
 std::atomic<uint64_t> checksum{0};
 
-struct StringCopyFromCache {
-  // This test copies the same string of size size1 to a new string
-  // object on the stack.
-  std::string x;
-  uint64_t size;
-  uint64_t pos = 0;
-  StringCopyFromCache(uint64_t size1, uint64_t) : x(size1, 'x'), size(size1) {
-    // Initialize to some funny values:
-    for (uint64_t i = 0; i < size1; ++i) {
-      x[i] = '0' + (char)(i % 78);
-    }
-  }
-  uint64_t work(uint64_t) {
-    std::string y = x;  // Make a copy
-    uint64_t r = y[pos];
-    pos += 47;
-    while (pos >= size) {
-      pos -= size;
-    }
-    return r;
-  }
-};
-
 struct StringCopy {
   // This test creates size1 random strings of size size2 and then copies
   // random ones over others.
@@ -57,7 +34,7 @@ struct StringCopy {
     }
   }
   uint64_t work(uint64_t) {
-    v[pos1] = v[pos2];
+    v[pos1] = v[pos2];  // Let's assume that this does not do a memory alloc
     pos1 += 12341;
     if (pos1 >= size1) {
       pos1 = pos1 % size1;
@@ -75,18 +52,19 @@ struct Substr {
   // and then copies a pseudo random subrange of size `size2` to some
   // other string variable using the substr method.
   std::string s;
+  std::string sub;
   uint64_t size1;
   uint64_t size2;
   uint64_t pos;
-  std::string sub;
   Substr(uint64_t size1, uint64_t size2) : size1(size1), size2(size2), pos(0) {
     s.reserve(size1);
     for (uint64_t i = 0; i < size1; ++i) {
       s.push_back('0' + (char)(i % 78));
     }
+    sub.reserve(size2);
   }
   uint64_t work(uint64_t) {
-    sub = s.substr(pos, size2);
+    sub = s.substr(pos, size2);  // Let's assume that this does alloc
     pos += 47153;
     if (pos > size1 - size2) {
       pos = pos % (size1 - size2);
@@ -221,6 +199,44 @@ struct MemSet {
   }
 };
 
+struct MemCmp {
+  // This test allocates a memory area of size `size1` bytes and fills it
+  // with the same byte. Then it uses memcmp to compare two regions of size
+  // `size2` in it.
+  std::vector<char> v;
+  uint64_t size1;
+  uint64_t size2;
+  uint64_t pos1;
+  uint64_t pos2;
+  MemCmp(uint64_t size1, uint64_t size2)
+      : size1(size1), size2(size2), pos1(0), pos2(1234567 % (size1 - size2)) {
+    if (size2 * 100 > size1) {
+      std::cout << "size1 must be 100x larger than size2!" << std::endl;
+      std::abort();
+    }
+    if (pos2 < size2) {
+      // Just to move it away from pos1
+      pos2 += 47 * size2;
+    }
+    v.reserve(size1);
+    for (uint64_t i = 0; i < size1; ++i) {
+      v.push_back('x');
+    }
+  }
+  uint64_t work(uint64_t) {
+    int r = memcmp(&v[pos2], &v[pos1], size2);
+    pos1 += 47153;
+    if (pos1 > size1 - size2) {
+      pos1 = pos1 % (size1 - size2);
+    }
+    pos2 += 97251;
+    if (pos2 > size1 - size2) {
+      pos2 = pos2 % (size1 - size2);
+    }
+    return v[pos1] + r;
+  }
+};
+
 void header() {
   std::cout << fmt::format(
       ".-{:^30s}---{:^10s}---{:^10s}---{:^12s}---{:^12s}---{:^12s}-.\n",
@@ -305,14 +321,6 @@ int main(int argc, char* argv[]) {
 
   header();
 
-  measure<StringCopyFromCache>("string_copy_from_cache", 32, 0);
-  measure<StringCopyFromCache>("string_copy_from_cache", 80, 0);
-  measure<StringCopyFromCache>("string_copy_from_cache", 257, 0);
-  measure<StringCopyFromCache>("string_copy_from_cache", 1234, 0);
-  measure<StringCopyFromCache>("string_copy_from_cache", 1024 * 1024 + 17, 0);
-
-  divider();
-
   measure<StringCopy>("string_copy", 100001, 32);
   measure<StringCopy>("string_copy", 100001, 80);
   measure<StringCopy>("string_copy", 100001, 257);
@@ -366,29 +374,42 @@ int main(int argc, char* argv[]) {
   measure<MemSet>("memset", 1024 * 1024 * 1024 + 17, 1234);
   measure<MemSet>("memset", 1024 * 1024 * 1024 + 17, 40763);
 
+  divider();
+
+  measure<MemCmp>("memset", 1024 * 1024 + 17, 32);
+  measure<MemCmp>("memset", 1024 * 1024 + 17, 80);
+  measure<MemCmp>("memset", 1024 * 1024 + 17, 257);
+  measure<MemCmp>("memset", 1024 * 1024 + 17, 1234);
+  measure<MemCmp>("memset", 1024 * 1024 * 1024 + 17, 32);
+  measure<MemCmp>("memset", 1024 * 1024 * 1024 + 17, 1234);
+  measure<MemCmp>("memset", 1024 * 1024 * 1024 + 17, 40763);
+
   footer();
 
   std::cout << R"(Explanations:
-    string_copy_from_cache:
-        Copies a single fixed string of length s1 to a new string object
-        on the stack, probably all in cache.
     string_copy:
-        Creates s1 strings of length s2 and then copies a random one to another
-        random one.
+        Creates s1 strings of length s2 and then copies a random one to
+        another random one. Tries to avoid allocations.
     substr:
-        Creates a string of length s1 and then uses substr to copy substrings
-        of length s2 out to a string variable.
+        Creates a string of length s1 and then uses substr to copy
+        substrings of length s2 out to a string variable. Tries to avoid
+        allocations.
     strlen:
         Creates s1 strings of length s2 and then uses strlen on random ones.
     memcpy:
-        Creates a memory area of size s1 and then uses memcpy to copy some random
-        area of size s2 to another one (non-overlapping).
-    memmove:
-        Creates a memory area of size s1 and then uses memmove to copy some random
-        area of size s2 to another one (possibly overlapping).
+        Creates a memory area of size s1 and then uses memcpy to copy
+        some random area of size s2 to another one (non-overlapping).
+        memmove: Creates a memory area of size s1 and then uses memmove to
+        copy some random area of size s2 to another one (possibly
+        overlapping).
     memset:
-        Creates a memory area of size s1 and then uses memset to set some random area
-        of size s2 to a random byte value.
+        Creates a memory area of size s1 and then uses memset to set
+        some random area of size s2 to a random byte value.
+    memcmp:
+        This test allocates a memory area of size `size1` bytes and
+        fills it with the same byte. Then it uses memcmp to compare two
+        regions of size `size2` in it. That is, all comparisons are
+        equal.
 )";
   return 0;
 }
